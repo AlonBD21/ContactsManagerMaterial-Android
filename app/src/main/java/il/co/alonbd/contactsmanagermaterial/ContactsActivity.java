2635246b2a385b7e24d42970d89fe7ac3ac4e3f2
AlonBD <alonbd21@gmail.com>
2019-08-19 14:16:21 +0300

    setup

---
+++ app/src/main/java/il/co/alonbd/contactsmanagermaterial/ContactsActivity.java
@@ -0,0 +1,13 @@
+package il.co.alonbd.contactsmanagermaterial;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ContactsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contacts);
+    }
+}
