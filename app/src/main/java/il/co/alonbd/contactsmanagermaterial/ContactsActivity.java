033cd7ee49eabd87bd22d9d47709d97e353f2a31
AlonBD <alonbd21@gmail.com>
2019-08-20 23:15:25 +0300

    basic construction, actionbar with home button added

--- app/src/main/java/il/co/alonbd/contactsmanagermaterial/ContactsActivity.java
+++ app/src/main/java/il/co/alonbd/contactsmanagermaterial/ContactsActivity.java
@@ -1,13 +1,32 @@
 package il.co.alonbd.contactsmanagermaterial;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class ContactsActivity extends AppCompatActivity {
-
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.baseline_menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home){
+            Toast.makeText(this, "Home Selected!", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
