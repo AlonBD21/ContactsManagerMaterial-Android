df3fd1ffdcc7c77a8334640b4af123ab1e22b49c
Alon Ben Dov <alonbd21@gmail.com>
2019-08-25 20:25:50 +0300

    Dayone (#1)
    * basic construction, actionbar with home button added
    
    * Main Menu added
    
    Added menu - can open both with swipe and with the hamburger icon of the actionbar.

--- app/src/main/java/il/co/alonbd/contactsmanagermaterial/ContactsActivity.java
+++ app/src/main/java/il/co/alonbd/contactsmanagermaterial/ContactsActivity.java
@@ -1,13 +1,35 @@
 package il.co.alonbd.contactsmanagermaterial;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class ContactsActivity extends AppCompatActivity {
-
+    DrawerLayout rootDrawer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        rootDrawer = findViewById(R.id.drawer_root);
+
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.baseline_menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home){
+            rootDrawer.openDrawer(Gravity.START);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
