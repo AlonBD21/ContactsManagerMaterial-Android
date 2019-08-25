b55b262d7fbaf1d932aaba6f9b187181a2d443c4
AlonBD <alonbd21@gmail.com>
2019-08-25 20:20:07 +0300

    Main Menu added
    Added menu - can open both with swipe and with the hamburger icon of the actionbar.

--- app/src/main/java/il/co/alonbd/contactsmanagermaterial/ContactsActivity.java
+++ app/src/main/java/il/co/alonbd/contactsmanagermaterial/ContactsActivity.java
@@ -1,22 +1,25 @@
 package il.co.alonbd.contactsmanagermaterial;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.MenuItem;
 import android.widget.Toast;
 
 public class ContactsActivity extends AppCompatActivity {
-    Toolbar toolbar;
+    DrawerLayout rootDrawer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts);
 
-        toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        rootDrawer = findViewById(R.id.drawer_root);
 
+        setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setHomeAsUpIndicator(R.drawable.baseline_menu);
@@ -25,7 +28,7 @@ public class ContactsActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == android.R.id.home){
-            Toast.makeText(this, "Home Selected!", Toast.LENGTH_SHORT).show();
+            rootDrawer.openDrawer(Gravity.START);
         }
         return super.onOptionsItemSelected(item);
     }
